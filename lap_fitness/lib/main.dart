46eca82a8249943f68338ed7cf721dbac3fb76af
Darrian <darrianyang978@gmail.com>
2023-02-27 15:50:01 -0500

    changed color again

--- lap_fitness/lib/main.dart
+++ lap_fitness/lib/main.dart
@@ -10,20 +10,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var themeData = ThemeData(
+      // This is the theme of your application.
+      //
+      // Try running your application with "flutter run". You'll see the
+      // application has a blue toolbar. Then, without quitting the app, try
+      // changing the primarySwatch below to Colors.green and then invoke
+      // "hot reload" (press "r" in the console where you ran "flutter run",
+      // or simply save your changes to "hot reload" in a Flutter IDE).
+      // Notice that the counter didn't reset back to zero; the application
+      // is not restarted.
+      primarySwatch: Colors.green,
+    );
     return MaterialApp(
       title: 'LAP',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Color.fromARGB(255, 17, 229, 176),
-      ),
+      theme: themeData,
       home: const MyHomePage(title: 'LAP'),
     );
   }
