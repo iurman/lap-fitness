72d7d92aecf94057531252ee55ec9a03e4b02a58
Darrian <darrianyang978@gmail.com>
2023-03-27 23:54:36 -0400

    added assets folder to branch

--- lap_fitness/lib/main.dart
+++ lap_fitness/lib/main.dart
@@ -1,75 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'login_page.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
-void main() {
-  runApp(MaterialApp(home: MyApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: 'AIzaSyBKVvcV9EPJL_OXx3I1p6z4forM-kAiqEg',
+          appId: '1:1076168022651:web:c48ba297634030ba68a0de',
+          messagingSenderId: '1076168022651',
+          projectId: 'lap-fitness',
+          authDomain: 'lap-fitness.firebaseapp.com',
+          databaseURL: 'https://lap-fitness-default-rtdb.firebaseio.com',
+          storageBucket: 'lap-fitness.appspot.com',
+          measurementId: 'G-476VRMEZBX'),
+    );
+  } catch (e) {
+    // ignore: avoid_print
+    print('Error initializing Firebase: $e');
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Page Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                hintText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text('Login'),
-              onPressed: () {
-                // TODO: Add login logic
-                String email = _emailController.text;
-                String password = _passwordController.text;
-                print('Email: $email\nPassword: $password');
-              },
-            ),
-            SizedBox(height: 20.0),
-            SignInButton(
-              Buttons.Google,
-              onPressed: () {
-                // TODO: Add Google sign-in logic
-                print('Google Sign-In');
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lap_fitness/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lap_fitness/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_auth
+import firebase_core
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
