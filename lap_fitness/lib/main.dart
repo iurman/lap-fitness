d42bcfb0f42296cd5c5c80986c5ae982f76884b7
iurman <116571634+iurman@users.noreply.github.com>
2023-04-04 12:13:05 -0400

    Updated app icon

--- lap_fitness/lib/main.dart
+++ lap_fitness/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MainPage(),
