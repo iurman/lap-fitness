7f5f8202af4ba34cb65c5bf4b85f9129736d075a
iurman <116571634+iurman@users.noreply.github.com>
2023-04-17 12:57:35 -0400

    Update loading_page.dart

--- lap_fitness/lib/loading_page.dart
+++ lap_fitness/lib/loading_page.dart
@@ -4,28 +4,56 @@ import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'package:lap_fitness/home_page.dart';
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:lap_fitness/user_info.dart';
 
 class LoadingPage extends StatefulWidget {
   final String welcomeMessage;
 
-  const LoadingPage({Key? key, required this.welcomeMessage}) : super(key: key);
+  LoadingPage({required this.welcomeMessage});
 
   @override
   _LoadingPageState createState() => _LoadingPageState();
 }
 
 class _LoadingPageState extends State<LoadingPage> {
+  late DatabaseReference _userRef;
+  late User _currentUser;
+
   @override
   void initState() {
     super.initState();
 
-    Timer(Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => HomePage(),
-        ),
-      );
+    _currentUser = FirebaseAuth.instance.currentUser!;
+    _userRef = FirebaseDatabase.instance
+        .reference()
+        .child('users')
+        .child(_currentUser.uid);
+
+    _userRef.onValue.first.then((DatabaseEvent userEvent) {
+      final user = userEvent.snapshot.value as Map<dynamic, dynamic>?;
+      if (user != null &&
+          user['age'] != null &&
+          user['gender'] != null &&
+          user['weight'] != null &&
+          user['height'] != null &&
+          user['calories'] != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  UserInfoPage(calories: widget.welcomeMessage)),
+        );
+      }
+    }).catchError((error) {
+      // handle error
     });
   }
 
@@ -33,11 +61,17 @@ class _LoadingPageState extends State<LoadingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        CircularProgressIndicator(color: Color.fromARGB(255, 138, 104, 35)),
-        SizedBox(height: 16),
-        Text(widget.welcomeMessage),
-      ])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              color: Color.fromARGB(255, 138, 104, 35),
+            ),
+            SizedBox(height: 16),
+            Text(widget.welcomeMessage),
+          ],
+        ),
+      ),
     );
   }
 }
