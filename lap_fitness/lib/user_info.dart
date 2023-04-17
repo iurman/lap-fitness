3fc510ee3457c937990e3f60339e4833205ee3a7
iurman <116571634+iurman@users.noreply.github.com>
2023-04-17 14:14:30 -0400

    Inputs not saving fixed

--- lap_fitness/lib/user_info.dart
+++ lap_fitness/lib/user_info.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:lap_fitness/home_page.dart';
+import 'package:flutter/services.dart';
 
 class UserInfoPage extends StatefulWidget {
   final String? calories;
@@ -22,7 +23,13 @@ class _UserInfoPageState extends State<UserInfoPage> {
   final _weightController = TextEditingController();
   final _heightController = TextEditingController();
   final _calorieController = TextEditingController();
+  final _heightFeetController = TextEditingController();
+  final _heightInchesController = TextEditingController();
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _selectedGender;
+  final List<String> _genders = ['Male', 'Female', 'Non-binary', 'Other'];
+
   late final _caloriesController = TextEditingController(
       text: widget
           .calories); // Assign the passed calorie amount to a new controller
@@ -43,9 +50,11 @@ class _UserInfoPageState extends State<UserInfoPage> {
       final user = event.snapshot.value as Map<dynamic, dynamic>;
       setState(() {
         _ageController.text = user['age'] ?? '';
-        _genderController.text = user['gender'] ?? '';
+        _selectedGender =
+            user['gender'] ?? ''; // Update _selectedGender instead
         _weightController.text = user['weight'] ?? '';
-        _heightController.text = user['height'] ?? '';
+        _heightFeetController.text = user['heightFeet'] ?? '';
+        _heightInchesController.text = user['heightInches'] ?? '';
         _calorieController.text = user['calories'] ?? '';
       });
     }, onError: (error) {
@@ -58,7 +67,8 @@ class _UserInfoPageState extends State<UserInfoPage> {
     _ageController.dispose();
     _genderController.dispose();
     _weightController.dispose();
-    _heightController.dispose();
+    _heightFeetController.dispose(); // Dispose of the new controller
+    _heightInchesController.dispose(); // Dispose of the new controller
     _calorieController.dispose();
     super.dispose();
   }
@@ -66,9 +76,10 @@ class _UserInfoPageState extends State<UserInfoPage> {
   void _saveUserInfo() {
     // Get the form values
     final age = _ageController.text;
-    final gender = _genderController.text;
+    final gender = _selectedGender;
     final weight = _weightController.text;
-    final height = _heightController.text;
+    final heightFeet = _heightFeetController.text;
+    final heightInches = _heightInchesController.text;
     final calories = _calorieController.text;
 
     // Save the user info to the database
@@ -76,7 +87,8 @@ class _UserInfoPageState extends State<UserInfoPage> {
       'age': age,
       'gender': gender,
       'weight': weight,
-      'height': height,
+      'heightFeet': heightFeet,
+      'heightInches': heightInches,
       'calories': calories,
     }).then((_) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -111,27 +123,104 @@ class _UserInfoPageState extends State<UserInfoPage> {
                   keyboardType: TextInputType.number,
                 ),
                 SizedBox(height: 16),
-                Text('Gender'),
-                TextFormField(
-                  controller: _genderController,
+                DropdownButtonFormField<String>(
+                  value: _selectedGender,
+                  hint: Text('Select Gender'),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedGender = newValue;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please select your gender';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _genderController.text = value ?? '';
+                  },
+                  items: _genders.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
+
                 SizedBox(height: 16),
                 Text('Weight'),
+                // Weight TextFormField
                 TextFormField(
-                  controller: _weightController,
+                  controller: _weightController, // Add the controller here
                   keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                    hintText: 'Weight',
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 8.0), // Adjust the padding as needed
+                      child: Text('lbs'),
+                    ),
+                  ),
+                  // Implement the validator and onSaved logic
                 ),
+
                 SizedBox(height: 16),
                 Text('Height'),
-                TextFormField(
-                  controller: _heightController,
-                  keyboardType: TextInputType.number,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width *
+                          0.4, // Adjust the width as needed
+                      child: TextFormField(
+                        controller:
+                            _heightFeetController, // Add the controller here
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(
+                          hintText: 'Feet',
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8.0), // Adjust the padding as needed
+                            child: Text('ft'),
+                          ),
+                        ),
+                        // Implement the validator and onSaved logic
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width *
+                          0.4, // Adjust the width as needed
+                      child: TextFormField(
+                        controller:
+                            _heightInchesController, // Add the controller here
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(
+                          hintText: 'Inches',
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8.0), // Adjust the padding as needed
+                            child: Text('in'),
+                          ),
+                        ),
+                        // Implement the validator and onSaved logic
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 16),
                 Text('Target Calories'),
                 TextFormField(
                   controller: _calorieController,
                   keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
                 SizedBox(height: 16),
                 ElevatedButton(
@@ -154,7 +243,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
                       Color.fromARGB(255, 138, 104, 35),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
